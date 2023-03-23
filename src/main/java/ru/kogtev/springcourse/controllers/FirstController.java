38b49ff5f1b6aeb10f7c1c52bbce3a8325ad0695
Nikita Kogtev <nikitakogtev@gmail.com>
2023-03-23 21:24:28 +0700

    Third commit (before release calc)

--- src/main/java/ru/kogtev/springcourse/controllers/FirstController.java
+++ src/main/java/ru/kogtev/springcourse/controllers/FirstController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
+
 @Controller
 @RequestMapping("/first")
 public class FirstController {
@@ -22,8 +24,39 @@ public class FirstController {
     }
 
     @GetMapping("/goodbye")
-    public String goodByePage(){
+    public String goodByePage() {
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam("a") int a, @RequestParam("b") int b,
+                             @RequestParam("action") String action,
+                             Model model) {
+
+        double result;
+
+        switch (action) {
+            case ("multiplication"):
+                result = a * b;
+                break;
+            case ("addition"):
+                result = a + b;
+                break;
+            case ("subtraction"):
+                result = a - b;
+                break;
+            case ("division"):
+                result = a / (double) b;
+                break;
+            default:
+                result = 0;
+                break;
+        }
+
+        model.addAttribute("result", result);
+
+
+        return "first/calculator";
     }
 
 }
