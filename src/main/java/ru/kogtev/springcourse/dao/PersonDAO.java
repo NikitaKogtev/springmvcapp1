a29385329ad3a2a40709e6d18e0a09b419010c43
Nikita Kogtev <nikitakogtev@gmail.com>
2023-03-26 17:31:18 +0700

    Fourth commit (CRUD - READ)

---
+++ src/main/java/ru/kogtev/springcourse/dao/PersonDAO.java
@@ -0,0 +1,32 @@
+package ru.kogtev.springcourse.dao;
+
+import org.springframework.stereotype.Component;
+import ru.kogtev.springcourse.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(++PEOPLE_COUNT, "Nikita"));
+        people.add(new Person(++PEOPLE_COUNT, "Marina"));
+        people.add(new Person(++PEOPLE_COUNT, "Kate"));
+        people.add(new Person(++PEOPLE_COUNT, "Maria"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+    
+
+}
